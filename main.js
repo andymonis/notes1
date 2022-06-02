c9cd19f114da3ebe33dd660695f2ac658b07ea8b
Andy <andy@AM-Mac.local>
2022-06-02 16:03:24 +0100

    Simpliest App upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ * Notes App 
+ */
+export default class Main {
+    constructor(config) {
+    }
+
+    /**
+     * Main start point for the app
+     * @param {*} params
+     */
+    async init(params) {
+        console.log("Notes1")
+    }
+}
